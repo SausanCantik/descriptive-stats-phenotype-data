614affa098ec4aa8525eef0cefc180dcebda0ed0
SausanCantik <55223998+SausanCantik@users.noreply.github.com>
2019-09-13 10:11:33 +0700

    parameter : dataframe. it read the first column

--- 03. Null data treatment/string_split.py
+++ 03. Null data treatment/string_split.py
@@ -4,9 +4,11 @@ Creating A function to group the sample based on the accession
 split the sample name based on '-' and store it into variable called 'accession_group'
 '''
 
-def string_split (dataframe_column) :
+def string_split (dataframe) :
+    column_name = list(dataframe)
+    sample_ID = column_name[0]
     accession_group = []
-    for word in dataframe_column : 
+    for word in dataframe[sample_ID] : 
         split = word.split('-')
         accession_group.append(split[0])
         
